cc3a408a1b4b342a536e506f0316562e2d650f81
Shubham Bharti <107488607+shubhambharti4716@users.noreply.github.com>
2024-02-03 12:30:13 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Decimal Equivalent of Binary Linked List/decimal-equivalent-of-binary-linked-list.java
@@ -0,0 +1,106 @@
+//{ Driver Code Starts
+import java.util.*;
+class Node
+{
+    int data;
+    Node next;
+    Node(int d)
+    {
+        data = d;
+        next = null;
+    }
+}
+
+class LinkedList1
+{
+    Node head;  // head of lisl
+    /* Inserts a new Node at front of the list. */
+    public void addToTheLast(Node node)
+    {
+        if (head == null)
+        {
+            head = node;
+        }
+        else
+        {
+            Node temp = head;
+            while (temp.next != null)
+                temp = temp.next;
+
+            temp.next = node;
+        }
+    }
+    /* Function to print linked list */
+    void printList()
+    {
+        Node temp = head;
+        while (temp != null)
+        {
+            System.out.print(temp.data+" ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int t=sc.nextInt();
+        String s = sc.nextLine();
+        while(t>0)
+        {
+            int n = sc.nextInt();
+            String s1 = sc.nextLine();
+            LinkedList1 llist = new LinkedList1();
+
+            if (n > 0)
+            {
+                int a1=sc.nextInt();
+                Node head= new Node(a1);
+                llist.addToTheLast(head);
+            }
+            for (int i = 1; i < n; i++)
+            {
+                int a = sc.nextInt();
+                llist.addToTheLast(new Node(a));
+            }
+            System.out.println(new Solution().DecimalValue(llist.head));
+            t--;
+        }
+    }
+}
+
+
+// } Driver Code Ends
+
+
+/* Node of a linked list
+ class Node {
+   int data;
+    Node next;
+    Node(int d)  { data = d;  next = null; }
+}
+ Linked List class
+class LinkedList
+{
+    Node head;  // head of list
+}
+This is method only submission.  You only need to complete the method. */
+
+class Solution
+{
+    private static final long M = 1000000007;
+
+    long DecimalValue(Node head) {
+        long result = 0; // Initialize result to store the decimal value
+        Node current = head; // Start with the head of the linked list
+        
+        while (current != null) {
+            // For each node, shift the current result left (equivalent to multiplying by 2 in decimal),
+            // add the current node's data, and take modulo M to prevent overflow.
+            result = ((result * 2) % M + current.data) % M;
+            current = current.next; // Move to the next node
+        }
+        
+        return result;
+    }
+}
