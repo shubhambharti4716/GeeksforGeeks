2461f081da8765d83018104cadf280704fc12529
Shubham Bharti <107488607+shubhambharti4716@users.noreply.github.com>
2024-03-11 18:25:22 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Count pairs Sum in matrices/count-pairs-sum-in-matrices.java
@@ -0,0 +1,68 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String input[] = read.readLine().split(" ");
+            
+            int n = Integer.parseInt(input[0]);
+            int x = Integer.parseInt(input[1]);
+            
+            int mat1[][] = new int[n][n];
+            
+            for(int i = 0;i<n;i++){
+                input = read.readLine().split(" ");
+                for(int j = 0;j<n;j++){
+                    mat1[i][j] = Integer.parseInt(input[j]);
+                }
+            }
+            
+            int mat2[][] = new int[n][n];
+            
+            for(int i = 0;i<n;i++){
+                input = read.readLine().split(" ");
+                for(int j = 0;j<n;j++){
+                    mat2[i][j] = Integer.parseInt(input[j]);
+                }
+            }
+            
+        
+            
+            Solution ob = new Solution();
+            System.out.println(ob.countPairs(mat1,mat2,n,x));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    int countPairs(int mat1[][], int mat2[][], int n, int x) {
+        // code here
+        HashSet<Integer> set = new HashSet<>();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                set.add(mat2[i][j]);
+            }
+        }
+        int ans = 0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                int val = x - mat1[i][j];
+                if(set.contains(val)){
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+}
