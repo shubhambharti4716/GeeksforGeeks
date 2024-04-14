4d19f2e9a8b5111dc2bbe39aefa3463ab8f40384
Shubham Bharti <107488607+shubhambharti4716@users.noreply.github.com>
2024-04-14 23:51:45 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Xoring and Clearing/xoring-and-clearing.java
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+import java.util.*;
+
+public class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int arr[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+            Solution obj = new Solution();
+            obj.xor1ToN(n, arr);
+            obj.printArr(n, arr);
+            obj.setToZero(n, arr);
+            obj.printArr(n, arr);
+        }
+        sc.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+class Solution {
+    public void printArr(int n, int arr[]) {
+        // code here
+        for(int i : arr)System.out.print(i+" ");
+        System.out.println();
+    }
+
+    public void setToZero(int n, int arr[]) {
+        // code here
+        for(int i = 0; i < n ; i++){
+            arr[i] = 0;
+        }
+    }
+
+    public void xor1ToN(int n, int arr[]) {
+        // code here
+        for(int i = 0; i < n ; i++){
+            arr[i] ^= i;
+        }
+    }
+}
